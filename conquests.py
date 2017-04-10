9e49361d425a3e7d42c7b2f24d35b914927f58b7
jlaniau <laniau.julie@orange.fr>
2017-04-10 10:25:30 +0200

    Update conquests.py

--- conquests.py
+++ conquests.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     parser.add_argument("-b","--biomass", help='identifiant of biomass reaction', required=True)
     parser.add_argument("-s","--seed", help='list of boundary seeds in SBML format for topological search', default="")
     parser.add_argument("-t","--target", help='list of biomass compounds in SBML format', default="")
-    parser.add_argument('-l', '--limit', help='limit to flux value to be considered as null', type=float, default=1E-5)
+    parser.add_argument('-l', '--limit', help='limit to flux value to be considered as null (by default 10E-5)', type=float, default=1E-5)
     parser.add_argument('-r',"--repository", help="repository to store the answer-files (by default named answer_file in draft repository)",default="")
 
 
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     if not os.path.isdir(repository) :
         os.makedirs(repository)
     
-    # recuperation des seed et targets 
+    
     seed_target_sbml.seed_target_from_sbml(draft, biomass_id,repository)
     seed = repository+"seeds_from_sbml.sbml"
     target = repository+"targets_from_sbml.sbml"
@@ -48,9 +48,9 @@ if __name__ == '__main__':
         topo_crossroad.topological_crossroad(draft, seed, target, repository)
     else :
         topo_crossroad.topological_crossroad(draft, seed_cofactor, target, repository)
-    #topo_cross = repository+'topological_crossroads.csv'
+   
     
     stoichio_crossroad.stoichio_crossroad(draft, draft, seed, target, limit, repository)
-    #stochio_cross = repository+"stoichiometrical_crossroads.csv"
+    
     
     optimal_crossroad.optimal_crossroad(draft, seed, target, limit, repository)
